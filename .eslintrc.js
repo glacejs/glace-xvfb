93cb60341935b9d4ccf2cd8170077932c3948a6e
Sergei Chipiga <chipiga86@gmail.com>
2018-03-23 20:16:07 +0200

    Add workflow commands

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "globals": {
+        "SS": true,
+        "expect": true,
+        "chunk": true,
+        "test": true,
+        "sinon": true,
+        "CONF": true,
+        "beforeChunk": true,
+        "afterChunk": true,
+        "fxXvfb": true,
+        "after": true,
+        "before": true,
+        "scope": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2017,
+        "sourceType": "module"
+    },
+    "rules": {
+        "no-console": 0,
+        "no-extra-semi": 0,
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
