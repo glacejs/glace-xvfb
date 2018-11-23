727c578f0d5b206bd35d5af95483c55d6811aa5e
Sergei Chipiga <sergei.chipiga@pipedrive.com>
2018-11-23 10:13:59 +0200

    Use fixtures factory

--- tests/e2e/tests.js
+++ tests/e2e/tests.js
@@ -1,7 +1,7 @@
 "use strict";
 
 suite("e2e tests", () => {
-    test("launch test in xvfb", null, [fxXvfb], () => {
+    test("launch test in xvfb", [fxXvfb], () => {
         chunk(async () => {
             await $.pause(1, "sleep");
         });
